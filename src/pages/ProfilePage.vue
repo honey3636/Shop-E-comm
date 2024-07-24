0865369e4b3e05a03022efe1bbb4db85c54c4df3
honey3636 <hhhhoney2003@gmail.com>
2024-07-23 19:35:18 -0800

    add getUser Method in ProfilePage

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,26 +1,38 @@
 <template>
-<NavBar2/>
+    <NavBar2 />
 
-<div class="2xl:container mx-auto  ">
-<h1 class="text-green-900 mt-[70px] text-3xl " >Profile</h1>
-
-</div>
+    <div class="2xl:container mx-auto  ">
+        <h1 class="text-green-900 mt-[70px] text-3xl ">Profile</h1>
+    </div>
+    <div>Name:{{ user }}</div>
 </template>
 
 <!-- script section -->
 <script>
 import NavBar2 from "@/components/NavBar2.vue";
 
-export default{
-    name:"ProfilePage",
-    components:{
+export default {
+    name: "ProfilePage",
+    components: {
         NavBar2,
-    }
-
+    },
+    data() {
+        return {
+            user: [],
+        }
+    },
+    methods: {
+        getUser() {
+            let person = localStorage.getItem("user-info");
+            // this.user = JSON.parse(person);
+            this.user = person;
+        }
+    },
+    mounted() {
+        this.getUser();
+    },
 }
 </script>
 
 <!-- style section -->
-<style>
-
-</style>
+<style></style>
